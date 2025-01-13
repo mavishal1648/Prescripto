66531672ea88b28f2fbaa7a493f43fb2cb9bf031
mavishal1648 <mavishal1648@gmail.com>
2025-01-13 12:08:11 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/home';
+import Home from './pages/Home';
 
 const App = () => {
   return (
